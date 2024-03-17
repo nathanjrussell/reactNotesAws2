afe2d6644fb7399fcddc486198c270298567f2c6
Nathan Russell <nathan@blackpathway.com>
2024-03-17 18:03:09 -0400

    new stuff

---
+++ test/react_notes_aws2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ReactNotesAws2 from '../lib/react_notes_aws2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/react_notes_aws2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ReactNotesAws2.ReactNotesAws2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

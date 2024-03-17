351b22893a14f0fdf91df4d6ce058b120946a436
Nathan Russell <nathan@blackpathway.com>
2024-03-17 18:35:10 -0400

    added environment

--- lib/react_notes_aws2-stack.ts
+++ lib/react_notes_aws2-stack.ts
@@ -1,11 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { env } from 'process';
 export class ReactNotesAws2Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    env: {account: '360228089988'},
     const lambdaFunction = new lambda.Function(this, 'LambdaFunction', {
-      environment: {account: '360228089988'},
       runtime: lambda.Runtime.PYTHON_3_12,
       code: lambda.Code.fromAsset('lambda'),
       handler: "main.handler"

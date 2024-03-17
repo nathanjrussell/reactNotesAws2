782a3b076f27669f8d7d494f019eef11114b101f
Nathan Russell <nathan@blackpathway.com>
2024-03-17 18:15:37 -0400

    new stuff

--- lib/react_notes_aws2-stack.ts
+++ lib/react_notes_aws2-stack.ts
@@ -1,16 +1,27 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 export class ReactNotesAws2Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const lambdaFunction = new lambda.Function(this, 'LambdaFunction', {
+      runtime: lambda.Runtime.PYTHON_3_12,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: "main.handler"
+    });
 
-    // The code that defines your stack goes here
+    const functionUrl = lambdaFunction.addFunctionUrl( {
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ['*'],
+        allowedMethods: [lambda.HttpMethod.ALL],
+        allowedHeaders: ['*'],
+      }
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ReactNotesAws2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new cdk.CfnOutput(this, 'Url', { 
+      value: functionUrl.url,
+    });
   }
+
 }

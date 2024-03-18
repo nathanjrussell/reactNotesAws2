917d64fde293659cd22f7595f9bfa62d1fa3d695
Nathan Russell <nathan@blackpathway.com>
2024-03-17 21:10:09 -0400

    yes

--- lib/react_notes_aws2-stack.ts
+++ lib/react_notes_aws2-stack.ts
@@ -1,8 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import { env } from 'process';
 export class ReactNotesAws2Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, { 
@@ -15,28 +13,6 @@ export class ReactNotesAws2Stack extends cdk.Stack {
       handler: "main.handler"
     });
 
-    /**
-     * Represents the API Gateway for the React Notes application.
-     */
-    const api = new apigateway.RestApi(this, 'ApiGateway', {
-      restApiName: 'ReactNotesApiGateway',
-      description: 'This is my lambda function',
-    });
-
-    const notesResource = api.root.addResource('notes');
-    notesResource.addMethod('GET', new apigateway.LambdaIntegration(lambdaFunction));
-
-       // Create a deployment of the API
-       const deployment = new apigateway.Deployment(this, 'ApiDeployment', {
-        api: api,
-      });
-      const stageName = 'prod-' + Date.now();
-      // Associate the deployment with a stage
-    // Create a stage and associate it with the deployment
-    new apigateway.Stage(this, 'ProdStage', {
-      deployment: deployment,
-      stageName: stageName,
-    });
   }
 
 }

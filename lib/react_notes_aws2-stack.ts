14478f84c76627185e398182c108c53bc1018388
Nathan Russell <nathan@blackpathway.com>
2024-03-17 21:45:28 -0400

    added descriptions

--- lib/react_notes_aws2-stack.ts
+++ lib/react_notes_aws2-stack.ts
@@ -6,11 +6,14 @@ import * as iam from 'aws-cdk-lib/aws-iam'; // Import IAM
 export class ReactNotesAws2Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, { 
-      ...props, env: {account: '360228089988'}
+        ...props, 
+        env: {account: '360228089988'},
+        description: "The AWS CDK stack for the React Notes App"
     });
 
     const lambdaRole = new iam.Role(this, 'LambdaRole', {
       assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+      description: "Role for the react notes app lambda functions to read from the DynamoDB table",
     });
     const policyName = 'arn:aws:iam::360228089988:policy/service-role/AWSLambdaMicroserviceExecutionRole-254f80b2-c0c1-402a-9974-6b1d31eab1e6';
     lambdaRole.addManagedPolicy(iam.ManagedPolicy.fromManagedPolicyArn(this, 'CustomPolicy', policyName));
@@ -20,6 +23,7 @@ export class ReactNotesAws2Stack extends cdk.Stack {
       code: lambda.Code.fromAsset('lambda'),
       handler: "getMenu.lambda_handler",
       role: lambdaRole, // Assign the role here
+      description: "returns the menu for the react notes app"
     });
 
   }
